d319b13681a80e5e90db4e97fdbf5fa4eff0c756
Bryan Hernandez <bryan79297@gmail.com>
2024-04-21 13:25:32 -0500

    4/21/24
    Implemented weapon fire sounds (Shooting, Reload, and empty fire).

--- FinalProject/Assets/c# Files/PlayerInputHandler.cs
+++ FinalProject/Assets/c# Files/PlayerInputHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,11 @@ public class PlayerInputHandler : MonoBehaviour
     [SerializeField] creature playerCreature;
     ProjectileThrower projectileThrower;
     private Animator _animator;
-    //public static Text ammoText;
+    public AudioSource GunShot;
+    public AudioSource Reload;
+    public AudioSource EmptyFire;
+
+
     public Text textComponent; //testing UI string change
 
     static int x = 0;
@@ -50,6 +55,7 @@ public class PlayerInputHandler : MonoBehaviour
             //checks Reserves b4 shooting
             if (Remaining != 0){
                 projectileThrower.Launch(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                GunShot.Play();
                 //ammoText.text = "Ammo: " + Remaining + "/" + Max;
                 Remaining -= 1;
               
@@ -57,13 +63,18 @@ public class PlayerInputHandler : MonoBehaviour
             else if (Remaining == 0){
                 //N/A ammo message
                 Debug.Log("Out of Ammo - *CLICK*");
+                EmptyFire.Play();
             }
         }
         if (Input.GetKeyDown(KeyCode.R)){ 
                 Chamber(); //Starts Reload
+                Reload.Play();
             }
          //Gun reload code^^^^^-----------------------------------------------------------------------
     }
+
+
+
     static void Chamber(){
                 Debug.Log("Updating Ammo");
                 Debug.Log(Remaining);
@@ -79,82 +90,3 @@ public class PlayerInputHandler : MonoBehaviour
                 }
     }
 }
-//-----------------------------------------------------------My Own code-------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//-----------------------------------------------------------ChatGPT code------------------------------------------------------------------
-/*
-public class PlayerInputHandler : MonoBehaviour
-{
-    [SerializeField] creature playerCreature;
-    ProjectileThrower projectileThrower;
-    private Animator _animator;
-    [SerializeField] static Text ammoText; // Serialized field for the UI text
-    static int x = 0;
-    [SerializeField] static int Max = 10;
-    [SerializeField] static int Remaining = 4;
- 
-    void Start()
-    {
-        projectileThrower = playerCreature.GetComponent<ProjectileThrower>();
-        _animator = GetComponent<Animator>();
-        UpdateAmmoDisplay(); // Update the UI text on start
-    }
-
-    void Update()
-    {
-        _animator.SetBool("IsWalking", x != 1);
-
-        Vector3 input = Vector3.zero;
-
-        if (Input.GetButton("Horizontal"))
-        {
-            Debug.Log("Horizontal Movement Pressed");
-            input.x = Input.GetAxis("Horizontal");
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            Debug.Log("Jump Button Pressed");
-            playerCreature.Jump();
-        }
-        playerCreature.MoveCreature(input);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Firing Primary");
-            if (Remaining != 0)
-            {
-                projectileThrower.Launch(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-                Remaining -= 1;
-                UpdateAmmoDisplay(); // Update UI text after shooting
-            }
-            else if (Remaining == 0)
-            {
-                Debug.Log("Out of Ammo - *CLICK*");
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Chamber();
-        }
-    }
-
-    static void UpdateAmmoDisplay()
-    {
-        ammoText.text = "Ammo: " + Remaining + "/" + Max;
-    }
-
-    static void Chamber()
-    {
-        Debug.Log("Updating Ammo");
-        if (Remaining != Max)
-        {
-            Remaining += 1;
-            UpdateAmmoDisplay(); // Update UI text after reloading
-        }
-        else if (Remaining == Max)
-        {
-            Debug.Log("Max Ammo Capacity");
-        }
-    }
-}
-*/
